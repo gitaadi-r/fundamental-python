e399dd25b0a54474086c47c1c02038d839a77aa6
Gita Stark <higitaadi@gmail.com>
2020-07-30 07:51:48 +0700

    percabangan

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import requests
+#Percabangan
+is_fast = True
 
-print('Hello World')
-
-r = requests.get ('https://google .com')
-
-try:
-    if r.status_code == 200:
-        print(r.text)
-except Exception as e:
-    print('Error', e)
+if is_fast:
+    print('fast route')
+else:
+    print('slow route')
