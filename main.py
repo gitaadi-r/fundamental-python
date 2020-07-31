bc01ec6d5d745d51d0df6417ec45bc6d2961eb4f
Gita Stark <higitaadi@gmail.com>
2020-08-01 00:55:40 +0700

    tipe data list, perulangan list

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ jumlah_anak = 4
 
 for index in range(1, jumlah_anak+1):
     print(f'Halo anak ke {index}')
+
+#tipe data list
+anak = ['eko', 'dhani', 'kay', 'donna']
+print(anak)
+anak.append('gita')
+print(anak)
+
+#perulangan list
+for a in anak:
+    print(f'Hi {a}')
+
+#perulangan cara 2
+for a in range(0, len(anak)):
+    print(f'{a+1}. Hi {anak[a]}')
